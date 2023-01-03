d1f3e28ea6151101cfe617d7a826bfff78b06e2a
Chris <cochsenreither@protonmail.com>
2023-01-02 21:39:23 -0700

    started class InstanceConfig

--- config.py
+++ config.py
@@ -1,82 +1,94 @@
-INSTANCE_CONFIG = {
-    "IDENTITY_TYPE": None,
-    "InstanceAlias": "",
-    "TELEPHONY": {
-        "INCOMING": False,
-        "OUTGOING": False
-    },
-    "DATA": {
-        "RECORDINGS": {
-            "CUSTOM": False
-        },
-        "TRANSCRIPTS": {
-            "CUSTOM": False
-        },
-        "REPORTS": {
+from enums import IdType
+from shared.util import Util
 
-        },
-        "ATTACHMENTS": {
-            "CUSTOM": False
-        }
-    },
-    "CLOUDWATCH": {
-        "CF_LOGS": False
-    }
-}
+class InstanceConfig:
 
-IDENTITY_CONFIGS = (
-    {
-        "ORDINAL": "1",
-        "CHOICE": "CONNECT_MANAGED",
-        "DISPLAY": "Store users in Amazon Connect",
-        "ADMIN": False,
-        "ADMIN_FIELDS": {
-            "FIRSTNAME": {
-                "DISPLAY": "First name: ",
-                "VALUE": ""
-            },
-            "LASTNAME": {
-                "DISPLAY": "Last name: ",
-                "VALUE": ""
-            },
-            "USERNAME": {
-                "DISPLAY": "Username: ",
-                "VALUE": ""
-            },
-            "PASSWORD": {
-                "DISPLAY": "Password: ",
-                "VALUE": ""
-            },
-            "PASSWORD_VERIFY": {
-                "DISPLAY": "Password (verify): ",
-                "VALUE": ""
-            }
-        }
-    },
-    {
-        "ORDINAL": "2",
-        "CHOICE": "EXISTING_DIRECTORY",
-        "DISPLAY": "Link to an existing directory",
-        "ADMIN": False
-    },
-    {
-        "ORDINAL": "3",
-        "CHOICE": "SAML",
-        "DISPLAY": "SAML 2.0-based authentication",
-        "ADMIN": False,
-        "ADMIN_FIELDS": {
-            "FIRSTNAME": {
-                "DISPLAY": "First name: ",
-                "VALUE": ""
-            },
-            "LASTNAME": {
-                "DISPLAY": "Last name: ",
-                "VALUE": ""
-            },
-            "USERNAME": {
-                "DISPLAY": "Username: ",
-                "VALUE": ""
-            },            
-        }
-    }
-)
+    def __init__(self, **config_dict):
+        if not Util.is_dict_empty(config_dict):
+            for k, v in config_dict.items():
+                setattr(self, k, v)
+
+    
+
+# INSTANCE_CONFIG = {
+#     "IDENTITY_TYPE": None,
+#     "InstanceAlias": "",
+#     "TELEPHONY": {
+#         "INCOMING": False,
+#         "OUTGOING": False
+#     },
+#     "DATA": {
+#         "RECORDINGS": {
+#             "CUSTOM": False
+#         },
+#         "TRANSCRIPTS": {
+#             "CUSTOM": False
+#         },
+#         "REPORTS": {
+
+#         },
+#         "ATTACHMENTS": {
+#             "CUSTOM": False
+#         }
+#     },
+#     "CLOUDWATCH": {
+#         "CF_LOGS": False
+#     }
+# }
+
+# IDENTITY_CONFIGS = (
+#     {
+#         "ORDINAL": "1",
+#         "CHOICE": "CONNECT_MANAGED",
+#         "DISPLAY": "Store users in Amazon Connect",
+#         "ADMIN": False,
+#         "ADMIN_FIELDS": {
+#             "FIRSTNAME": {
+#                 "DISPLAY": "First name: ",
+#                 "VALUE": ""
+#             },
+#             "LASTNAME": {
+#                 "DISPLAY": "Last name: ",
+#                 "VALUE": ""
+#             },
+#             "USERNAME": {
+#                 "DISPLAY": "Username: ",
+#                 "VALUE": ""
+#             },
+#             "PASSWORD": {
+#                 "DISPLAY": "Password: ",
+#                 "VALUE": ""
+#             },
+#             "PASSWORD_VERIFY": {
+#                 "DISPLAY": "Password (verify): ",
+#                 "VALUE": ""
+#             }
+#         }
+#     },
+#     {
+#         "ORDINAL": "2",
+#         "CHOICE": "EXISTING_DIRECTORY",
+#         "DISPLAY": "Link to an existing directory",
+#         "ADMIN": False
+#     },
+#     {
+#         "ORDINAL": "3",
+#         "CHOICE": "SAML",
+#         "DISPLAY": "SAML 2.0-based authentication",
+#         "ADMIN": False,
+#         "ADMIN_FIELDS": {
+#             "FIRSTNAME": {
+#                 "DISPLAY": "First name: ",
+#                 "VALUE": ""
+#             },
+#             "LASTNAME": {
+#                 "DISPLAY": "Last name: ",
+#                 "VALUE": ""
+#             },
+#             "USERNAME": {
+#                 "DISPLAY": "Username: ",
+#                 "VALUE": ""
+#             },            
+#         }
+#     }
+# )
