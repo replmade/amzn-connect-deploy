b80d5eccd583f0a7d81f6806ae30103aefa9524f
Chris <cochsenreither@protonmail.com>
2023-01-07 20:11:19 -0700

    moved options parser into separate file

--- main.py
+++ main.py
@@ -1,31 +1,23 @@
-from optparse import OptionParser
+from options import get_options
 from guide import Guide
 from deployer import Deployer
 from shared.clients import ClientFactory
 
 if __name__ == '__main__':
-    parser = OptionParser()
-    parser.add_option('-p', '--profile', dest='profile_name', default="default",
-        help='The AWS credentials profile to use for the install')
-    parser.add_option('-r', '--region', dest='region_name', default="us-east-1",
-        help='The AWS region to create the Connect instance (overrides profile region)')
-    parser.add_option('-c', '--config', dest='config', default=None,
-        help='A configuration file that specifies the Connect instance settings')
-    (options, args) = parser.parse_args()
 
-    user_type = None
-    instance_alias = None
-    use_admin = False
-    admin_fields = None
+    # Get the command line options for the script
+    (options, args) = get_options()
 
     # Create boto3 clients
     client_factory = ClientFactory(options.profile_name, options.region_name)
     client_factory.create_all_clients()
 
+    # If no config was passed to the script, run the guided setup
     if not options.config:
         guide = Guide()
         instance_config = guide.get_create_options()
 
+    # Initialize Deployer and deploy the instance
     deployer = Deployer()
     deployer.run_create(client_factory.connect_client, instance_config)
 
