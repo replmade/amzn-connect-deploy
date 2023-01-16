1f97ee4c05000d3d07f7ebdd558155b09c953bae
Chris <cochsenreither@protonmail.com>
2023-01-16 16:33:52 -0700

    parse arguments with type declarations

--- cdk-scripts/lib/parse-options.ts
+++ cdk-scripts/lib/parse-options.ts
@@ -1,58 +1,20 @@
-const allowedParams: string[] = [
-    'attributes',
-    'identityManagementType',
-    'directoryId',
-    'instanceAlias',
-];
+import { CfnInstance, CfnInstanceProps } from 'aws-cdk-lib/aws-connect';
 
-const allowedAttr = [
-    'inboundCalls',
-    'outboundCalls',
-    'autoResolveBestVoices',
-    'contactflowLogs',
-    'contactLens',
-    'earlyMedia',
-    'useCustomTtsVoices',
-];
-
-type InstanceParams = {
-    identityManagementType: string,
-    attributes: any,
-    instanceAlias?: string,
-    directoryId?: string,
-};
-
-type AttributeParams = {
-    inboundCalls: boolean,
-    outboundCalls: boolean,
-    autoResolveBestVoices?: boolean,
-    contactLens?: boolean,
-    contactflowLogs?: boolean,
-    earlyMedia?: boolean,
-    useCustomTtsVoices?: boolean,
-};
-
-const getParamsFromArgs = (): any => {
-    let params: any = {};
-    let attrs: any = {};
+const getinstancePropsFromArgs = (): CfnInstanceProps => {
     const argv = require('minimist')(process.argv.slice(2));
 
-    for (let arg in argv) {
-        if (allowedParams.indexOf(arg) > -1) {
-            params[arg] = argv[arg];
-        }
+    const instanceAttrs = <CfnInstance.AttributesProperty>{
+        inboundCalls: false,
+        outboundCalls: false,
+        ...argv,
+    };
 
-        if (allowedAttr.indexOf(arg) > -1) {
-            if (params.attributes === undefined) {
-                params['attributes'] = {};
-            }
+    const instanceProps = <CfnInstanceProps>{
+        identityManagementType: 'CONNECT_MANAGED',
+        attributes: instanceAttrs,
+        ...argv,
+    };
 
-            params.attributes[arg] = argv[arg];
-        }
-    }
-
-    const instanceParams = params as InstanceParams;
-    instanceParams.attributes = attrs as AttributeParams;
-    return instanceParams;
+    return instanceProps;
 }
-module.exports = getParamsFromArgs;
+module.exports = getinstancePropsFromArgs;
