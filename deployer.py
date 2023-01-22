f0a82afde4f539d3e80f519e69f3686943888a28
Christopher Ochsenreither <cochsenreither@protonmail.com>
2023-01-21 21:23:16 -0700

    call cdk bootstrap for switching profiles

--- deployer.py
+++ deployer.py
@@ -11,15 +11,17 @@ class Deployer:
         # Write the JSON file the CDK script 'cdk-scripts-stack.js' will read
         Util.write_json_file(instance_props, './cdk-scripts/lib/instance-props.json')
 
+        os.chdir('./cdk-scripts')
+        # Bootstrap CDK with profile
+        os.system(f'cdk bootstrap --profile {profile}')
+
         # Run cdk synth
         template_name = f"{instance_props['instanceAlias']}.yaml"
         synth_cmd = f'cdk synth > {template_name}'
-        os.chdir('./cdk-scripts')
-        synth_resp = os.system(synth_cmd)
-        print(synth_resp)
+        os.system(synth_cmd)
 
+        # Deploy instance
         deploy_cmd = f'cdk deploy --profile {profile}'
-        deploy_resp = os.system(deploy_cmd)
-        print(deploy_resp)
+        os.system(deploy_cmd)
 
         return True
