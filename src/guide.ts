b2b98488bea55671b1bf2430d1bde44ad47d460e
Christopher Ochsenreither <replmade@gmail.com>
2023-05-30 22:04:36 -0700

    better empty input handling

--- src/guide.ts
+++ src/guide.ts
@@ -1,48 +1,16 @@
 import { CfnInstanceProps } from 'aws-cdk-lib/aws-connect';
-import { input, confirm } from './shared/util';
-
-export enum IdentityManagementType {
-    CONNECT_MANAGED = 1,
-    EXISTING_DIRECTORY,
-    SAML,
-};
-
-interface AwsConfig {
-    profile: string,
-    region: string,
-};
-
-export interface GuideResponse {
-    cfnInstanceProps: CfnInstanceProps,
-    awsConfig: AwsConfig,
-};
-
-const validConnectRegions: string[] = [
-    'us-east-1',
-    'us-west-2',
-    'ap-northeast-1',
-    'ap-northeast-2',
-    'ap-southeast-1',
-    'ap-southeast-2',
-    'ca-central-1',
-    'eu-central-1',
-    'eu-central-2',
-    'eu-west-2',
-    'eu-south-1',
-    'eu-south-2',
-    'af-south-1',
-    'ap-east-1',
-    'ap-south-2',
-    'ap-southeast-3',
-    'ap-southeast-4',
-    'me-south-1',
-    'me-central-1',
-];
+import { input, confirm, emptyNullUndefined } from './shared/util';
+import {
+    IdentityManagementType,
+    AwsConfig,
+    GuideResponse,
+    validConnectRegions,
+} from './datatypes';
 
 export class Guide {
     static async #handleConfirm(option: any) {
-        const confirmation = await confirm('Is this correct? ');
-        if (confirmation) {
+        const confirmation = await confirm(`Is [${option}] correct? `);
+        if (confirmation === true) {
             return option;
         } else {
             return null;
@@ -50,8 +18,11 @@ export class Guide {
     }
 
     static async setProfile(): Promise<string> {
-        let profile = 'default';
-        profile = await input('Set AWS profile [Default]: ');
+        let profile;
+        profile = await input('Set AWS profile [default]: ');
+        if (profile === '') {
+            profile = 'default';
+        }
         return this.#handleConfirm(profile);
     }
 
@@ -59,7 +30,11 @@ export class Guide {
         let region = 'us-east-1';
         region = await input('Set deployment region [us-east-1]: ');
         while (!validConnectRegions.includes(region)) {
-            console.log(`Connect cannot be deployed to region ${region}`);
+            if (region === '') {
+                console.log('No region was entered.');
+            } else {
+                console.log(`Connect cannot be deployed to region '${region}'`);
+            }
             region = await input('Set deployment region [us-east-1]: ');
         }
         return this.#handleConfirm(region);
@@ -98,14 +73,14 @@ export class Guide {
         return this.#handleConfirm(aliasName.toLowerCase());
     }
 
-    static async allowCalls(promptMessage: string): Promise<boolean | null> {
+    static async selectBooleanOption(promptMessage: string): Promise<boolean | null> {
         let response = null;
         while (typeof response !== 'boolean') {
-          response = await confirm(promptMessage);
+            response = await confirm(promptMessage);
         }
         console.log(`You entered ${response ? 'yes' : 'no'}.`);
-        return this.#handleConfirm(response);
-      }
+        return this.#handleConfirm(response);        
+    }
 
     static async chooseDirectoryId(): Promise<string | undefined> {
         let id = '';
@@ -123,31 +98,48 @@ export class Guide {
         let identityManagementType: string = '';
         let instanceAlias: string = '';
         let directoryId: string | undefined = undefined;
-        let outbound: boolean | null = null;
-        let inbound: boolean | null = null;
+        let outboundCalls: boolean | null = null;
+        let inboundCalls: boolean | null = null;
+        let contactLens: boolean | null = null;
+        let contactflowLogs: boolean | null = null;
+        let autoResolveBestVoices: boolean | null = null;
 
-        awsProfile = await Guide.setProfile();
+        while (emptyNullUndefined(awsProfile)) {
+            awsProfile = await Guide.setProfile();   
+        }
 
-        awsRegion = await Guide.setRegion();
+        while (emptyNullUndefined(awsRegion)) {
+            awsRegion = await Guide.setRegion();
+        }
 
-        while (identityManagementType === '') {
+        while (emptyNullUndefined(identityManagementType)) {
             identityManagementType = await Guide.setIdentityManagement();        
         }
-        console.log(identityManagementType);
-        while (instanceAlias === '') {
+
+        while (emptyNullUndefined(instanceAlias)) {
             instanceAlias = await Guide.chooseInstanceAlias();
         }
+
         if (identityManagementType === 'EXISTING_DIRECTORY') {
             while (directoryId === null) {
                 directoryId = await Guide.chooseDirectoryId();
             }
         }
-        while (outbound === null) {
-            outbound = await Guide.allowCalls('Allow outbound calls? ');
+        while (outboundCalls === null) {
+            outboundCalls = await Guide.selectBooleanOption('Allow outbound calls? ');
         }
-        while (inbound === null) {
-            inbound = await Guide.allowCalls('Allow inbound calls? ');
+        while (inboundCalls === null) {
+            inboundCalls = await Guide.selectBooleanOption('Allow inbound calls? ');
         }
+        while (contactLens === null) {
+            contactLens = await Guide.selectBooleanOption('Enable Contact Lens? ');
+        }
+        while (contactflowLogs === null) {
+            contactflowLogs = await Guide.selectBooleanOption('Enable contact flow logs? ');
+        }
+        while (autoResolveBestVoices === null) {
+            autoResolveBestVoices = await Guide.selectBooleanOption('Use the best available voice from Amazon Polly? ');
+        } 
 
         const awsConfig: AwsConfig = {
             profile: awsProfile,
@@ -159,8 +151,11 @@ export class Guide {
             instanceAlias,
             directoryId,
             attributes: {
-                inboundCalls: inbound,
-                outboundCalls: outbound,
+                inboundCalls,
+                outboundCalls,
+                contactLens,
+                contactflowLogs,
+                autoResolveBestVoices,
             }
         }
 
